48ef06339e03fd5072787cfebafe2a9bfd0adbe8
Vladislav Klimenko <sunsingerus@gmail.com>
2026-02-10 14:38:52 +0500

    dev: minor test

--- pkg/controller/chi/controller_test.go
+++ pkg/controller/chi/controller_test.go
@@ -39,8 +39,8 @@ func Test_shouldEnqueue(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := shouldEnqueue(tt.chi); got != tt.want {
-				t.Errorf("shouldEnqueue() = %v, want %v", got, tt.want)
+			if got := ShouldEnqueue(tt.chi); got != tt.want {
+				t.Errorf("ShouldEnqueue() = %v, want %v", got, tt.want)
 			}
 		})
 	}
